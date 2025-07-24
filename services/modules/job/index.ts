3e886bba6f723ea59ee5f9cd2700fe49523c113a
qvinh <doanquangvinh69@gmail.com>
2025-07-24 22:27:41 +0700

    update new feature

--- services/modules/job/index.ts
+++ services/modules/job/index.ts
@@ -1,12 +1,23 @@
 import type { ApiResponse } from '~/types/api'
 import type FetchFactory from '~/services/factory'
-import type { UserEntity } from '~/entities/user'
 
 const JobModule = (apiService: FetchFactory) => {
-  const getJob = async () => apiService.get<ApiResponse<any>>(ROUTE_API.JOB.GET_JOB)
+  const getJob = async () =>
+    apiService.get<ApiResponse<any>>(ROUTE_API.JOB.GET_JOB)
+
+  const searchJob = async (params: Record<string, any>) => {
+    // Gọi $fetch trực tiếp để giữ nguyên camelCase
+    const config = useRuntimeConfig()
+
+    return await $fetch(config.public.apiUrl + '/jobs/search', {
+      method: 'GET',
+      params,
+    })
+  }
 
   return {
     getJob,
+    searchJob,
   }
 }
 
