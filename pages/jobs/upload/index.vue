da188f7faedb993d26c33d6326dd273001a0e999
khaCrz <80761070+khaCrz@users.noreply.github.com>
2025-11-05 16:21:26 +0700

    Change mst assignment to use company ID instead

--- pages/jobs/upload/index.vue
+++ pages/jobs/upload/index.vue
@@ -1905,7 +1905,7 @@ const findCompanyByMst = async () => {
           // Fill data from VietQR
           companyAdd.value.name = companyByMST.value.data.name ?? ''
           companyAdd.value.address = companyByMST.value.data.address ?? ''
-          companyAdd.value.mst = companyByMST.value.data.mst ?? ''
+          companyAdd.value.mst = companyByMST.value.data.id ?? ''
 
           // Init default data for new company
           companyAdd.value.logo = ''
